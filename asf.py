e728952d7e63286f878aeb8990d01c73c678640f
orange <orange@format.com>
2021-02-26 12:31:44 +0800

    asf

---
+++ asf.py
@@ -0,0 +1,126 @@
+input = ["MYARTLOLLIPOPS MYLARBALLOONS",
+"MASSACHUSETTSBAYCOLONY MINUTEMANNATIONALHISTORICALPARK",
+"LOWERMACTOWNSHIPPA CRANBERRYTOWNSHIPPA",
+"AMERICANCOMPUTERSCIENCELEAGUE NATIONALACADEMICGAMESLEAGUE",
+"ABCDEFGHIJK ABDCEFGKILKJMN"]
+
+
+test0 = "abcdeft abxcgbtzfp"
+test1 = "blameablenesses blamelessnesses"
+test2 = "mezzamines razzmatazz"
+
+def get_list(input):
+    a, b = input.split(" ")
+    a_list = []
+    b_list = []
+    for item in a:
+        a_list.append(item)
+    for item in b:
+        b_list.append(item)
+    return a_list,b_list
+
+def del_same(a:list,b:list):
+    temp_a = []
+    temp_b = []
+    length = len(a) if len(b) > len(a) else len(b)
+    for i in range(length):
+        if(a[i] != b[i]):
+            temp_a.append(a[i])
+            temp_b.append(b[i])
+
+    temp_b = temp_b + b[len(a)::]
+    return temp_a,temp_b
+
+def align(a:list,b:list):
+    if len(b) > len(a):
+        for i in range(len(a)):
+            if (a[i] == b[i + 1]):
+                del b[i]
+                return a, b, True
+            if i < len(a) -1:
+                if (b[i] == a[i + 1]):
+                    del a[i]
+                    return a, b, True
+    elif len(b) < len(a):
+        for i in range(len(b)):
+            if i < len(b) - 1:
+                if (a[i] == b[i + 1]):
+                    del b[i]
+                    return a, b, True
+
+            if (b[i] == a[i + 1]):
+                del a[i]
+                return a, b, True
+    else:
+        for i in range(len(a)-1):
+            if (a[i] == b[i+1]):
+                del b[i]
+                return a,b,True
+
+            if (b[i] == a[i+1]):
+                del a[i]
+                return a,b,True
+    return a,b,False
+
+
+
+def cal_asf(a:list,b:list):
+    result = abs(len(a) - len(b))
+    length = len(a) if len(b) > len(a) else len(b)
+    for i in range(length):
+        temp = ord(a[i]) - ord(b[i])
+        result += temp
+    print("asf value is: {}".format(result))
+
+
+
+def cal_result(a,b):
+    c = True
+    while (c):
+        a,b = del_same(a,b)
+        print("a: ", a)
+        print("b: ", b)
+        print("***********************")
+
+        a,b,c = align(a,b)
+        if c:
+            print("a: ", a)
+            print("b: ", b)
+            print("***********************")
+
+        else:
+            print("result")
+            print("a: ", a)
+            print("b: ", b)
+            print("***********************")
+            break
+
+    cal_asf(a,b)
+
+if __name__ == "__main__":
+    # a,b = get_list(input[0])
+    print("test 0")
+    a,b = get_list(test0)
+    cal_result(a, b)
+    print()
+
+
+    print("test 1")
+    a, b = get_list(test1)
+    cal_result(a, b)
+    print()
+
+    print("test 2")
+    a, b = get_list(test2)
+    cal_result(a, b)
+    print()
+
+    print("start calculate!!!")
+    for i in range(5):
+        print("case: {}".format(i))
+        a, b = get_list(input[i])
+        print("a: ", a)
+        print("b: ", b)
+        cal_result(a, b)
+        print()
+
--- ensure_lib.py
+++ ensure_lib.py
@@ -2,14 +2,62 @@ def ensure_lib(lib_name:list):
     for item in lib_name:
         print("ensure_lib               ./libraries/{}/".format(item))
         print("vmap     {}               ./libraries/{}/".format(item,item))
-
-a = [
+b = [
+"work",
+"work_lib",
+"altera_ver",
+"lpm_ver",
+"sgate_ver",
+"altera_mf_ver",
+"altera_lnsim_ver",
+"twentynm_ver",
+"twentynm_hssi_ver",
+"twentynm_hip_ver",
+"altera",
+"lpm",
+"sgate",
+"altera_mf",
+"altera_lnsim",
+"twentynm",
+"twentynm_hssi",
+"twentynm_hip",
 "pcie_ed_tb",
 "altera_pcie_a10_tbed_191",
 "DUT_pcie_tb_ip",
 "altera_common_sv_packages",
 "altera_conduit_bfm_191",
-"pcie_ed_inst_board_pins_bfm_ip",]
+"pcie_ed_inst_board_pins_bfm_ip",
+"altera_merlin_master_translator_191",
+"altera_merlin_slave_translator_191",
+"altera_merlin_master_agent_191",
+"altera_merlin_slave_agent_191",
+"altera_avalon_sc_fifo_1930",
+"altera_merlin_router_1920",
+"altera_avalon_st_pipeline_stage_1920",
+"altera_merlin_burst_adapter_1920",
+"altera_merlin_demultiplexer_1920",
+"altera_merlin_multiplexer_1920",
+"altera_mm_interconnect_1920",
+"altera_irq_mapper_1920",
+"altera_reset_controller_1920",
+"pcie_ed",
+"ast2avmm_bridge_256_191",
+"pcie_ed_APPS",
+"altpcie_devkit_191",
+"pcie_ed_DK",
+"altera_common_sv_packages",
+"altera_xcvr_native_a10_191",
+"altera_pcie_a10_hip_191",
+"altera_xcvr_fpll_a10_191",
+"altera_xcvr_atx_pll_a10_191",
+"pcie_ed_DUT",
+"altera_avalon_onchip_memory2_1920",
+"pcie_ed_MEM",
+]
+
+def vsim(lib_name:list):
+    for item in lib_name:
+        print("-L {}".format(item),end=" ")
 
 
-ensure_lib(a)
+vsim(b)
